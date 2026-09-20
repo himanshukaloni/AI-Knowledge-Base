d844739c48cd12d3dc49dcc7a05ee95434ee749d
Himanshu Kaloni <himanshukaloni99@gmail.com>
2026-09-20 16:14:33 +0530

    logger update

--- backend/config/logger.js
+++ backend/config/logger.js
@@ -2,6 +2,7 @@ const winston = require("winston");
 
 const logger = winston.createLogger({
   level: process.env.NODE_ENV === "production" ? "info" : "debug",
+
   format: winston.format.combine(
     winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
     winston.format.errors({ stack: true }),
@@ -10,12 +11,9 @@ const logger = winston.createLogger({
         `[${timestamp}] ${level.toUpperCase()}: ${stack || message}`
     )
   ),
+
   transports: [
-    new winston.transports.Console({
-      format: winston.format.combine(winston.format.colorize(), winston.format.simple()),
-    }),
-    new winston.transports.File({ filename: "logs/error.log", level: "error" }),
-    new winston.transports.File({ filename: "logs/combined.log" }),
+    new winston.transports.Console(),
   ],
 });
 
